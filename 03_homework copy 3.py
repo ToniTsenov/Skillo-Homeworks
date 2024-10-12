c5cba16731e59979dae2921863a27f3fdc7329bd
Toni Tsenov <cenov1990@gmail.com>
2024-10-13 00:35:08 +0300

    Update 03_homework copy 3.py

--- 03_homework copy 3.py
+++ 03_homework copy 3.py
@@ -22,12 +22,23 @@ print("The sum of all even numbers from 1 to 100 is:", sum_of_evens)
 '''
 # Problem 2: Write a Python script that prompts the user in the console a simple problem
 # ( how much does 5 + 17 equal to ) until the user provides a correct answer.
-num1 = random.randint(1, 100)
-num2 = random.randint(1, 100)
-total = num1 + num2
-user_answer = float(input(f"What is result of {num1} + {num2}: "))
-while True:
-    if user_answer == total:
-        print("You guess correct!")
-    else:
-        print("You wrong!Try again!")
+def random_sum_game():
+    while True:
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        total = num1 + num2
+
+        user_answer = int(input(f"What is the result of {num1} + {num2}: "))
+        
+        if user_answer == total:
+            print("You guessed correctly!")
+        else:
+            print(f"You are wrong! The correct answer is {total}. Try again!")
+
+        continue_game = input("Do you want to continue? (y/n): ").lower()
+
+        if continue_game != 'y':
+            print("Thanks for playing!")
+            break
+
+random_sum_game()
