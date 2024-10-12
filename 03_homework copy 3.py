0b00ef51caffdde6f45fb4a61fdf2bd22e06b54c
Toni Tsenov <cenov1990@gmail.com>
2024-10-12 23:15:38 +0300

    Update 03_homework copy 3.py

--- 03_homework copy 3.py
+++ 03_homework copy 3.py
@@ -10,12 +10,8 @@ else:
 # Problem 1: Write a Python program to find the sum of all even numbers from 1 to 100 using a loop.
 # Make use of control flow constructs like the for loop and conditional statements.
     sum_of_evens = 0
-
     for number in range(1, 101):
-    # Check if the number is even
         if number % 2 == 0:
-        # Add the even number to the sum
             sum_of_evens += number
 
-# Print the result
     print("The sum of all even numbers from 1 to 100 is:", sum_of_evens)
