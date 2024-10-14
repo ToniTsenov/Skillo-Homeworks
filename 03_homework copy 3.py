5328e248f861c1e1b060fdbbea4f37aed878043b
Toni Tsenov <cenov1990@gmail.com>
2024-10-14 17:53:08 +0300

    Update 03_homework copy 3.py

--- 03_homework copy 3.py
+++ 03_homework copy 3.py
@@ -42,7 +42,7 @@ def random_sum_game():
             break
 
 random_sum_game()
-'''
+
 # Problem 3: Write a Python script that iterates over the first 1000 numbers and prints "Fizz" if the number is divisible by 3,
 # "Buzz" if it's divisible by 5, and "FizzBuzz" if it's divisible by both 3 and 5.
 
@@ -55,3 +55,39 @@ for number in range(1, 1001):
         print(f"{number} Buzz")
     else:
         print(number)
+        '''
+# Problem 4: Design a Python program that simulates a simple guessing game.
+# The program should generate a random number between 1 and 100 and ask the user to guess it.
+# Provide hints like "Too high" or "Too low" until the user 
+# guesses the correct number. Use a while loop for this game.
+
+low_num = 1
+hight_num = 100
+answer = random.randint(low_num, hight_num)
+guesses = 0
+is_running = True
+
+print("Welcome to guess game!")
+print(f"Select number between {low_num} and {hight_num} ")
+
+while is_running:
+    guess = input("Enter the guess: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+        
+        if guess < low_num or guess > hight_num:
+            print("The number is out of range!")
+            print(f"Please select number between {lowest_num} and {highest_num} ")
+        elif guess < answer:
+            print("Too low!Try again!")
+        elif guess > answer:
+            print("Too hight!Try again!")
+        else:
+            print(f"You won!The answer was: {answer} ")
+            print(f"The number of guesses: {guesses}")
+            is_running = False
+    else:
+        print("Invalid guess!")
+        print(f"Please select number between {low_num} and {hight_num} ")
