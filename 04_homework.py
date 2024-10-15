b4d79b26d028204418774b82355db9958f3591cd
Toni Tsenov <cenov1990@gmail.com>
2024-10-15 23:28:04 +0300

    Update 04_homework.py

--- 04_homework.py
+++ 04_homework.py
@@ -15,7 +15,7 @@ print(sum_elements([100]))
 # Simple Calculator Function
 # Define a function called `simple_calculator` that takes two numbers and an operator ('+', '-', '*', or '/')
 # as arguments and returns the result of the operation.
-'''
+
 def simple_calculator(num1, num2, operator):
     if operator == "+":
         return num1 + num2
@@ -27,3 +27,9 @@ def simple_calculator(num1, num2, operator):
         return num1 / num2
 result = simple_calculator(2, 3, "*")
 print(result)
+'''
+# Problem 2: Area of Shapes
+# Create a module named `geometry` with functions to calculate the area of common shapes
+# like a square, rectangle, triangle, and circle. Import this module and use it to calculate the areas of
+# different shapes. You should be able to use the function for calculating the area of a rectangle to calculate
+# the area of a square by passing in only one argument.
