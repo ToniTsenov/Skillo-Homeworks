1608b10180d9a4d7f28957f971b13f65d1b93968
Toni Tsenov <cenov1990@gmail.com>
2024-10-15 23:25:26 +0300

    Update 04_homework.py

--- 04_homework.py
+++ 04_homework.py
@@ -1,3 +1,4 @@
+'''
 # Problem 0:
 # Complete the following function so that it returns the sum of the elements in the list passed as an argument.
 # Call your function several times in order to test it
@@ -14,3 +15,15 @@ print(sum_elements([100]))
 # Simple Calculator Function
 # Define a function called `simple_calculator` that takes two numbers and an operator ('+', '-', '*', or '/')
 # as arguments and returns the result of the operation.
+'''
+def simple_calculator(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+result = simple_calculator(2, 3, "*")
+print(result)
