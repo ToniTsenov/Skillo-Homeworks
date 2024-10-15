cbf5c19b7b176b7838a18e19cb404cda586006ba
Toni Tsenov <cenov1990@gmail.com>
2024-10-15 18:21:56 +0300

    Update 04_homework.py

--- 04_homework.py
+++ 04_homework.py
@@ -1,7 +1,11 @@
 # Problem 0:
-#Complete the following function so that it returns the sum of the elements in the list passed as an argument.
+# Complete the following function so that it returns the sum of the elements in the list passed as an argument.
 # Call your function several times in order to test it
-#def sum_elements(arr):
-# result = 0
- #insert code here
-# return result
+def sum_elements(arr):
+    result = 0
+    for element in arr:
+        result += element
+    return result
+print(sum_elements([1, 2, 3, 4, 5]))
+print(sum_elements([10, 20, 30]))
+print(sum_elements([100]))
