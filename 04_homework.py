47bcfb9604b87e1c6d70aeb5c72a762c5d544bc9
Toni Tsenov <cenov1990@gmail.com>
2024-10-15 17:03:14 +0300

    Create 04_homework.py

---
+++ 04_homework.py
@@ -0,0 +1,7 @@
+# Problem 0:
+#Complete the following function so that it returns the sum of the elements in the list passed as an argument.
+# Call your function several times in order to test it
+#def sum_elements(arr):
+# result = 0
+ #insert code here
+# return result
